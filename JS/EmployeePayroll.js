e4f06fd0eb405a3145865cdb56ba238c1eea66ea
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-25 18:41:37 +0530

    [Dipti] refactor code to extent class for employee payroll with gender,start date

--- JS/EmployeePayroll.js
+++ JS/EmployeePayroll.js
@@ -1,31 +1,42 @@
-//UC 11 EmployeePayrollData templates
+//UC 12 EmployeePayrollData templates
 class EmployeePayrollData{
     id;
-    name;
+    gender;
     salary;
+    //Uc 12 extent Refactored to add gender and salary
+    startDate;
     //constructor to store data
     //params to pass any number of parameter not forced to give all parameters
-    constructor(id,name,salary)
+    constructor(...params)
     { 
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1];
+        this.gender = params[2];
+        this.salary = params[3];
+        this.startDate = params[4];
     }
     
-    //Getter setter 
-    get name(){
+    //Getter and setter method
+    get name()
+    {
         return this._name;
     }
-    set name(value){
+    set name(value)
+    {
         this._name = value;     
     }
-    //Displaying the data in this format
-    toString(){
-        return "id = "+this.id+"\nName = "+this.name+"\nSalary = "+this.salary;
-    }
-}
 
+    toString()
+    {
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const empDate=this.startDate== undefined? "undefined":this.startDate.toLocaleDateString("en-US",options);
+        return "id = "+this.id+"\nName = "+this.name+"\nGender = "+this.gender+"\nSalary = "+this.salary+"\nStartdate = "+empDate;
+    }
+
+}
 let employeePayrollData = new EmployeePayrollData(1,"Diptimayee",30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = 'Jayant';
 console.log(employeePayrollData.toString());
+let newEmployee=new EmployeePayrollData(1,"Mahima","F",30000,new Date());
+console.log(newEmployee.toString());
