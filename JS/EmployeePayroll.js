2c1ef9e7e6939882331816ca6a0411a9d8c68e50
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-25 19:27:45 +0530

    [Dipti] added code to check for regex pattern matching for name

--- JS/EmployeePayroll.js
+++ JS/EmployeePayroll.js
@@ -1,9 +1,9 @@
-//UC 12 EmployeePayrollData templates
+//UC 13 EmployeePayrollData templates with regex expression
+
 class EmployeePayrollData{
     id;
     gender;
     salary;
-    //Uc 12 extent Refactored to add gender and salary
     startDate;
     //constructor to store data
     //params to pass any number of parameter not forced to give all parameters
@@ -23,7 +23,15 @@ class EmployeePayrollData{
     }
     set name(value)
     {
-        this._name = value;     
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(value))
+        {
+            this._name = value;
+        }
+        else 
+        {
+            throw "Incorrect name";   
+        } 
     }
 
     toString()
@@ -34,9 +42,26 @@ class EmployeePayrollData{
     }
 
 }
-let employeePayrollData = new EmployeePayrollData(1,"Diptimayee",30000);
-console.log(employeePayrollData.toString());
-employeePayrollData.name = 'Jayant';
-console.log(employeePayrollData.toString());
+
 let newEmployee=new EmployeePayrollData(1,"Mahima","F",30000,new Date());
 console.log(newEmployee.toString());
+
+try 
+{
+    newEmployee.name = "yamini";
+    console.log(employee.toString());
+} 
+catch (ex) 
+{
+    console.log("Pattern not matched")
+    console.error(ex);
+}
+try 
+{
+    let employee = new EmployeePayrollData(1,"Shashank",30000,"M",new Date());
+    console.log(employee.toString());
+}
+catch (ex) 
+{
+    console.error(ex);
+}
